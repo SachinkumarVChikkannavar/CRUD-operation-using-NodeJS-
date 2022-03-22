7e61f348d84da890e8f9f0b7d861eda78b4e6f5c
Sachinkumar V Chikkannavar <Sachinchikkannavar@gmail.com>
2022-03-23 01:44:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const coursesRouter=require("./routes/courses")
+const bodyParser=require('body-parser')
+require("dotenv").config()
+const mongoose=require('mongoose')
+const app=express();
+
+app.use(bodyParser.json)
+app.use("/api/v1/courses",coursesRouter)
+
+mongoose.connect(process.env.DB_CONNECTION_URL,() => {
+    console.log("Connected to db:")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log("server is running..");
+});
